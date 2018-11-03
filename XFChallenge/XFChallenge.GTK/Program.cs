0e920a1cab157406716ff7ac3560a2114a709245
Unknown <cbartonnh@gmail.com>
2018-11-02 21:53:11 -0400

    Mac and GTK

---
+++ XFChallenge/XFChallenge.GTK/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Gtk;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.GTK;
+
+namespace XFChallenge.GTK
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Gtk.Application.Init();
+            Forms.Init();
+
+            var app = new App();
+            var window = new FormsWindow();
+            window.LoadApplication(app);
+            window.SetApplicationTitle("Xamarin Forms Challenge");
+            window.Show();
+
+            Gtk.Application.Run();
+        }
+    }
+}
