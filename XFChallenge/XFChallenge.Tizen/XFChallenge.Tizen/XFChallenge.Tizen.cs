77ced65d8311f45c5e2f50da7ad3ee16be77fa78
Carl Barton <cbartonnh@gmail.com>
2018-11-02 20:02:23 -0400

    iOS Android UWP Tizen

---
+++ XFChallenge/XFChallenge.Tizen/XFChallenge.Tizen/XFChallenge.Tizen.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace XFChallenge.Tizen
+{
+    class Program : global::Xamarin.Forms.Platform.Tizen.FormsApplication
+    {
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            // Call 'LoadApplication(Application application)' here to load your application.
+            LoadApplication(new XFChallenge.App());
+        }
+
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            global::Xamarin.Forms.Platform.Tizen.Forms.Init(app);
+            app.Run(args);
+        }
+    }
+}
