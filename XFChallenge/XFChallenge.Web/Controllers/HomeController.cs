c4de91b8005e3efe6c4f1806d46ad5b123a2883a
Unknown <cbartonnh@gmail.com>
2018-11-02 23:40:51 -0400

    Web and added PowerPoint

---
+++ XFChallenge/XFChallenge.Web/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Ooui.AspNetCore;
+using Xamarin.Forms;
+using XFChallenge.Web.Models;
+
+namespace XFChallenge.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var page = new MainPage();
+            var element = page.GetOouiElement();
+            return new ElementResult(element, "Xamarin Forms Challenge");
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "The Xamarin Forms Challenge from GSCC 2018";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
